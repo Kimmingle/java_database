2cc185f7a1ebc6a04dbe8b9dd8a390d3fcad65c2
Kimmingle <t5328@naver.com>
2024-03-29 16:41:30 +0900

    db 새로 만듬

---
+++ test01/src/org/kh/dto/Controlfile.java
@@ -0,0 +1,19 @@
+package org.kh.dto;
+
+public class Controlfile {
+
+	private String name;
+	private String file_size_blks;
+	
+	private int block_size;
+	
+	private int blocks;
+	
+	
+	private int con_id;
+	
+	
+
+	
+	
+}
